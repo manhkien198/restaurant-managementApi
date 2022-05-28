f25cc091b4492c18593f76c6992b4b0df12a1641
manhkien198 <manhkien1998@gmail.com>
2022-05-28 23:10:17 +0700

    update

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-
 const app = express();
 const routes = require("./routes/index");
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 routes(app);
 module.exports = app;
