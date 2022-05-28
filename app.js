eaf54785b6d0084625d368c6113ed563e7bbacf1
manhkien198 <manhkien1998@gmail.com>
2022-05-29 00:02:43 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 const routes = require("./routes/index");
 app.use(express.json());
+app.use(cors());
 
 routes(app);
 module.exports = app;
