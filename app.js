4215ef473e4f1efbcacfee010a4a1c8d88c1fddb
manhkien198 <manhkien1998@gmail.com>
2022-05-29 00:16:23 +0700

    update

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const express = require("express");
 const app = express();
 const routes = require("./routes/index");
 app.use(express.json());
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
 routes(app);
 module.exports = app;
