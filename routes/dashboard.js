efabb3587eddd120abc5389745a7bf80a322ae5c
manhkien198 <manhkien1998@gmail.com>
2022-05-29 00:22:20 +0700

    update

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const auth = require("../middleware/auth");
 const router = express.Router();
-const app = express();
 const DashboardController = require("../controller/DashboardController");
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
 router.get("/", auth, DashboardController.index);
 module.exports = router;
