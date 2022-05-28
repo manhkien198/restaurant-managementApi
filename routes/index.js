51caf48e2fba814cc6c5c34721150a7088285b02
manhkien198 <manhkien1998@gmail.com>
2022-05-28 23:24:34 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const userRouter = require("./user");
 const roleRouter = require("./role");
 const dashboardRouter = require("./dashboard");
+const cors = require("cors");
 function routes(app) {
   app.use("/auth", userRouter);
   app.use("/roles", roleRouter);
-  app.use("/dashboard", dashboardRouter);
+  app.use("/dashboard", cors(), dashboardRouter);
 }
 
 module.exports = routes;
