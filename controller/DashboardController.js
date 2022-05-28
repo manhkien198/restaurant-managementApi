262d779907aa86a74607ddb81243cacb2fa7d7d4
manhkien198 <manhkien1998@gmail.com>
2022-05-28 23:41:35 +0700

    make it better

--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -2,6 +2,8 @@ const DashboardSchema = require("../model/dashboard");
 class DashboardController {
   //GET /role
   index(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
     DashboardSchema.find({})
       .then((data) => {
         res.json(data);
