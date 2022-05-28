f9ed8b9bd27aac59bbe1d607c21fe2949d8f83af
manhkien198 <manhkien1998@gmail.com>
2022-05-28 23:57:06 +0700

    update

--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -2,14 +2,6 @@ const DashboardSchema = require("../model/dashboard");
 class DashboardController {
   //GET /role
   index(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-
     DashboardSchema.find({})
       .then((data) => {
         res.json(data);
