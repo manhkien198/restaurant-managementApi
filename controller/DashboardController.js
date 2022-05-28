537e054747a8c082933151c60fc58e32a2e1c0c1
manhkien198 <manhkien1998@gmail.com>
2022-05-28 23:43:36 +0700

    make it better

--- controller/DashboardController.js
+++ controller/DashboardController.js
@@ -3,7 +3,15 @@ class DashboardController {
   //GET /role
   index(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
-
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    ); // If needed
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    ); // If needed
+    res.setHeader("Access-Control-Allow-Credentials", true); // If needed
     DashboardSchema.find({})
       .then((data) => {
         res.json(data);
