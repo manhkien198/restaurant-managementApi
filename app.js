353209f320d5e16b43bfb4cea0990d4774f4bfb1
manhkien198 <manhkien1998@gmail.com>
2022-05-29 00:29:04 +0700

    make it better

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const routes = require("./routes/index");
 app.use(express.json());
-app.all("/dashboard", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.use(cors());
+
 routes(app);
 module.exports = app;
