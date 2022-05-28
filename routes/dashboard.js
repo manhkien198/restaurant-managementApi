fc8e863eada1e09f461f9d3287cb3abf892813ee
manhkien198 <manhkien1998@gmail.com>
2022-05-28 23:40:28 +0700

    make it better

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const auth = require("../middleware/auth");
-const noCors = require("../middleware/noCors");
 const router = express.Router();
 const DashboardController = require("../controller/DashboardController");
 
-router.get("/", auth, noCors, DashboardController.index);
+router.get("/", auth, DashboardController.index);
 module.exports = router;
