1855ce0a2dea5e6bd67a771130a26c9f956b2964
dieptx@itop.io <manhkien1998@gmail.com>
2022-05-30 14:57:05 +0700

    update

--- controller/FoodController.js
+++ controller/FoodController.js
@@ -1,19 +1,37 @@
 const FoodSchema = require('../model/food');
 class FoodController {
   //GET /role
-  index(req, res, next) {
-    let perPage = 12;
-    let page = req.query.page || 1;
-    FoodSchema.find({})
-      .skip(perPage * page - perPage)
-      .limit(perPage)
-      .exec((err, food) => {
-        FoodSchema.countDocuments((err, count) => {
-          // đếm để tính có bao nhiêu trang
-          if (err) return next(err);
-          res.json(food); // Trả về dữ liệu các sản phẩm theo định dạng như JSON, XML,...
-        });
+  async index(req, res, next) {
+    try {
+      let { page, size, sort } = req.query;
+
+      // If the page is not applied in query.
+      if (!page) {
+        // Make the Default value one.
+        page = 1;
+      }
+
+      if (!size) {
+        size = 10;
+      }
+
+      //  We have to make it integer because
+      // query parameter passed is string
+      const limit = parseInt(size);
+
+      // We pass 1 for sorting data in
+      // ascending order using ids
+      const foods = await FoodSchema.find()
+        .sort({ votes: 1, _id: 1 })
+        .limit(limit);
+      res.send({
+        page,
+        size,
+        foods: foods,
       });
+    } catch (error) {
+      res.sendStatus(500);
+    }
   }
 }
 module.exports = new FoodController();
