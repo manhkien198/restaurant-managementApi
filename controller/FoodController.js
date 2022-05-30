0bcb92413ddddd24a7a556e780bc6a75898dd951
dieptx@itop.io <manhkien1998@gmail.com>
2022-05-30 14:30:43 +0700

    update

--- controller/FoodController.js
+++ controller/FoodController.js
@@ -1,12 +1,19 @@
-const FoodSchema = require("../model/food");
+const FoodSchema = require('../model/food');
 class FoodController {
   //GET /role
   index(req, res, next) {
+    let perPage = 12;
+    let page = req.query.page || 1;
     FoodSchema.find({})
-      .then((data) => {
-        res.json(data);
-      })
-      .catch(next);
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec((err, food) => {
+        FoodSchema.countDocuments((err, count) => {
+          // đếm để tính có bao nhiêu trang
+          if (err) return next(err);
+          res.json(food); // Trả về dữ liệu các sản phẩm theo định dạng như JSON, XML,...
+        });
+      });
   }
 }
 module.exports = new FoodController();
