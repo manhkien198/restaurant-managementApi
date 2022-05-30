1a3dabfbce95a46ffab5cf3517b101066ed0ad21
dieptx@itop.io <manhkien1998@gmail.com>
2022-05-30 16:17:36 +0700

    update

--- model/employee.js
+++ model/employee.js
@@ -4,6 +4,7 @@ const EmployeeSchema = new mongoose.Schema({
   fullname: String,
   position: String,
   role: String,
+  image:String
 });
 
 module.exports = mongoose.model('employee', EmployeeSchema);
