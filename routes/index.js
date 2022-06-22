95cc7a68a792800fb04c80538179edf5fecce318
kien pham <manhkien1998@gmail.com>
2022-06-22 13:42:45 +0700

    makeitbetter

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
-const userRouter = require("./user");
-const roleRouter = require("./role");
-const dashboardRouter = require("./dashboard");
-const foodRouter = require("./food");
-const employeeRouter = require("./employee");
-const productRouter = require("./products");
+const userRouter = require('./user');
+const roleRouter = require('./role');
+const dashboardRouter = require('./dashboard');
+const foodRouter = require('./food');
+const employeeRouter = require('./employee');
+const productRouter = require('./products');
+const categoryRouter = require('./categories');
 function routes(app) {
-  app.use("/auth", userRouter);
-  app.use("/roles", roleRouter);
-  app.use("/dashboard", dashboardRouter);
-  app.use("/food", foodRouter);
-  app.use("/employee", employeeRouter);
-  app.use("/products", productRouter);
+  app.use('/auth', userRouter);
+  app.use('/roles', roleRouter);
+  app.use('/dashboard', dashboardRouter);
+  app.use('/food', foodRouter);
+  app.use('/employee', employeeRouter);
+  app.use('/products', productRouter);
+  app.use('/categories', categoryRouter);
 }
 
 module.exports = routes;
