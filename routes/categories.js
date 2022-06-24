94132fff75034fa047a57fb065ddb348a704000d
kien pham <manhkien1998@gmail.com>
2022-06-24 16:34:13 +0700

    makeitbetter

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const auth = require('../middleware/auth');
 
 const router = express.Router();
 const CategoryController = require('../controller/CategoryController');
 
-router.get('/', auth, CategoryController.index);
+router.get('/', CategoryController.index);
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const auth = require('../middleware/auth');
 
 const router = express.Router();
 const ProductController = require('../controller/ProductController');
 
-router.get('/:category_id', auth, ProductController.showByFilter);
-router.get('/', auth, ProductController.index);
+router.get('/:category_id', ProductController.showByFilter);
+router.get('/', ProductController.index);
 
 module.exports = router;
